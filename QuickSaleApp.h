9f17b3a7558fd3a89fda8529723466f848ff7a5f
gustavo-dgs <58643975+gustavo-dgs@users.noreply.github.com>
2020-12-17 04:02:10 -0400

    Add files via upload

---
+++ QuickSaleApp.h
@@ -0,0 +1,21 @@
+/***************************************************************
+ * Name:      QuickSaleApp.h
+ * Purpose:   Defines Application Class
+ * Author:     ()
+ * Created:   2020-12-17
+ * Copyright:  ()
+ * License:
+ **************************************************************/
+
+#ifndef QUICKSALEAPP_H
+#define QUICKSALEAPP_H
+
+#include <wx/app.h>
+
+class QuickSaleApp : public wxApp
+{
+    public:
+        virtual bool OnInit();
+};
+
+#endif // QUICKSALEAPP_H
